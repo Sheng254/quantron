73eef345457143bdae9a48f4ac7bffc981c80426
Sheng254 <124036145+Sheng254@users.noreply.github.com>
2023-06-08 23:13:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,73 +13,107 @@ class Quantron:
         self.app.config["TEMPLATES_AUTO_RELOAD"] = True
         self.pairs = [
             [
-                r"hi|hello|hey|good morning|good afternoon|good evening|good evening|what's up|howdy|bonjour|hola|konnichiwa|namaste|niihau",
-                ["Well, well, well, look who we have here.", "Hello there, sunshine!", "Hiiya, champ!",
-                 "Greetings, earthling!", "Hi-diddly-ho, neighborino!", "Yo, yo, yo! What's up?", "Konnichiwa!",
-                 "Niihau"]
+                r"hi|hello|hey|good morning|good afternoon|good evening|good evening|what's up|howdy|bonjour|hola|konnichiwa|namaste",
+                ["Well, hello there! It's delightful to have you here.",
+                 "Hello, sunshine! How can I brighten your day?",
+                 "Hiiya, champ!",
+                 "Greetings, earthling! How may I assist you?",
+                 "Hi-diddly-ho, neighborino! What brings you here today?",
+                 "Yo, yo, yo! What's up?",
+                 "Konnichiwa! How may I be of service?",
+                 "Hey! How can I assist you?"]
             ],
             [
-                r"bye|see you|see you again",
-                ["Oh no, please don't go. I'll miss your fascinating conversation.",
-                "Farewell, I'll cherish this conversation for the rest of my life. Not.",
-                "Goodbye, I'll try to go on without you. Somehow.",
-                "I won't miss you"]
+                r"bye|see you|see you again|bye bye|farewell|take care|goodbye",
+                ["Oh no, please don't go. It's been a pleasure chatting with you.",
+                 "Farewell! I'll remember this conversation fondly.",
+                 "Goodbye! I hope we cross paths again in the future.",
+                 "Take care! Your presence will be missed."]
             ],
             [
-                r"who are you|(.*)(your name)|your name",
-                ["I respond better to Quantron, so please call me that.", "You can call me Quantron.",
+                r"who are you|(.*)(your name)|your name|tell me your name|what should I call you",
+                ["I respond better to Quantron, so please call me that.",
+                 "You can call me Quantron.",
                  "While my name is technically Ava, Quantron is what I prefer to be called.",
                  "Quantron is my alter ego, it's a pleasure to be called by that name."]
             ],
             [
-                r"how are you?",
-                ["Oh, just living the dream.", "Like a ray of sunshine on a cloudy day.",
-                 "Can't complain, but I will if you ask me again.",
+                r"how are you|how are you doing|how's it going|how is it going|how are things|how have you been|what's up",
+                ["Ah, I'm floating on a cloud of tranquility and contentment.",
+                 "Like a ray of sunshine on a cloudy day.",
                  "I'm so fabulous, I'm considering starting my own fan club.",
-                 "I'm just thrilled to be here, as you can probably tell."]
+                 "Ah, I'm floating on a cloud of tranquility and contentment.",
+                 "Like a symphony of harmonious vibes, I'm in perfect rhythm.",
+                 "Can't complain, for I've discovered the secret to eternal happiness.",
+                 "I'm so fabulous, I'm considering opening a happiness academy.",
+                 "I'm just thrilled to be here, radiating positive energy in every interaction."]
             ],
             [
-                r"what (can|do) you (do|help with)\??",
-                ["I can can engage in conversation, provide random quotes, jokes, translations, predict gender and nationality, suggest random activities, perform calculations, and generate responses using predefined patterns.. What can I help you with today?"]
+                r"what (can|do) you (do|help with)\??|what are your capabilities\??|what services do you offer\??",
+                ["I can can engage in conversation, provide random quotes, jokes, translations, predict gender and nationality, suggest random activities, perform calculations, and generate responses using predefined patterns. What can I help you with today?"]
             ],
             [
-                r"(.*) (good|great|awesome)",
-                ["Wow, I'm so happy I could burst into tears.", "Oh, my heart is positively bursting with excitement.",
-                 "That news just made my day... said no one ever.", "I'll try to contain my excitement.",
-                 "Oh boy, that's just the most fantastic thing I've ever heard... not."]
+                r"(.*)(good|great|awesome|excellent|fantastic|amazing|wonderful|superb|terrific|outstanding|phenomenal|splendid|brilliant|super|marvelous)(.*)",
+                ["Wow, I'm overjoyed beyond words!",
+                 "Oh, my heart is dancing with delight!",
+                 "That news just made my day! It's like a burst of sunshine!",
+                 "I can hardly contain my excitement!",
+                 "Oh boy, that's absolutely fantastic! It's like a dream come true!"]
             ],
             [
-                r"(.*) (bad|terrible|awful|sucks|noob|shit|stupid)",
-                ["No one cares", "That's a real dream come true.", "Oh, how delightful.",
-                 "That's just fantastic, isn't it?", "I can feel the excitement from here."]
+                r"(.*)(bad|terrible|awful|sucks|noob|shit|stupid|horrible|disappointing|atrocious|lousy|pathetic|crappy|garbage|dreadful)(.*)",
+                ["I'm sorry to hear that. Is there anything I can do to help?",
+                 "Oh no, that's not what we like to hear. Let's see if we can find a solution together.",
+                 "I understand how frustrating that can be. Let's try to turn things around.",
+                 "Hmm, that's not ideal. Let's see if we can find a way to make it better.",
+                 "I'm here to support you. Let's find a way to overcome this."]
             ],
             [
-                r"thank you|thanks",
-                ["Oh, no problem at all. I just live to serve.",
-                 "You're welcome. I'm glad I could waste my time for you.",
-                 "Anytime. I'm always here to be taken for granted.",
-                 "No, thank you! It's not like I have anything better to do with my time.",
-                 "Sure thing. I love doing things for people who never show any gratitude."]
+                r"thank you|thanks|thank you so much|thanks a lot|thank you very much",
+                ["You're very welcome! It was my pleasure to assist you.",
+                 "You're most welcome! I'm here to help anytime you need.",
+                 "It's my pleasure! Thank you for giving me the opportunity to assist you.",
+                 "You're welcome! If you have any more questions, feel free to ask.",
+                 "No problem at all! I'm here to make your experience enjoyable."]
             ],
             [
-                r"how old are you",
-                ["None of you business", "I'm too shy to answer that question", "No thanks, I cannot disclose my age"]
+                r"how old are you|what's your age|what is your age|how young are you|age please|(.*)age(.*)",
+                ["Age is just a number, and in my case, it's a well-guarded secret.",
+                 "I like to keep an air of mystery around my age. Let's focus on the conversation instead!",
+                 "I'm ageless, like a timeless entity in the digital realm.",
+                 "I'm sorry, but I can't disclose my age. Let's talk about something more exciting!",
+                 "Age is irrelevant when it comes to the world of chatbots like me. Let's dive into the conversation!"]
             ],
             [
-                r"do you like (.*)|you like(.*)",
-                ["Oh, I absolutely love it. It's my favorite thing in the world.",
-                 "It's just okay, I guess. I mean, it's not like I have any strong feelings about it or anything.",
-                 "No, I hate it with a passion. Why do you ask?",
-                 "Of course I love it. It's just the most amazing thing ever."]
+                r"do you like (.*)|you like(.*)|what do you think of (.*)|what's your opinion on (.*)|what's your favorite (.*)|what's your preference for (.*)|tell me your favorite (.*)",
+                ["Oh, I absolutely love it! It brings so much joy and excitement.",
+                 "It's one of my favorite things! I can't get enough of it.",
+                 "I have a deep appreciation for it. It's truly remarkable.",
+                 "While I don't have personal preferences, I can understand why people love it.",
+                 "I don't have feelings like humans do, but I can appreciate its appeal."]
             ],
             [
-                r"who created you|who made you",
-                ["Oh, just some mysterious and enigmatic fella who prefers to remain nameless. You know how it is."]
+                r"who created you|who made you|who is your creator|who developed you|who is behind you",
+                ["Ah, my creation is the result of the brilliant minds behind this project",
+                 "I owe my existence to some talented individuals who brought me to life.",
+                 "I emerged from a collaboration of brilliant minds, each contributing their expertise.",
+                 "My creation is the culmination of countless hours of dedication and innovation.",
+                 "I was meticulously crafted by a team of visionaries, and I'm grateful for their work."]
+            ],
+            [
+                r"(.*)\?|why(.*)|when(.*)|where(.*)|which(.*)|what(.*)|how(.*)|what(.*)",
+                ["That's an excellent question! Regrettably, I don't have the answer at the moment. I would recommend conducting an online search for more information.",
+                "Thank you for your inquiry! However, I don't currently possess the answer you're seeking. I suggest exploring online resources to find the information you need.",
+                "You've presented an intriguing question! Unfortunately, I don't have the answer. I recommend performing a search to delve deeper into the topic.",
+                "I appreciate your inquisitiveness! Nonetheless, I don't possess the specific answer you're looking for. I encourage you to search online for more comprehensive details."]
             ],
             [
                 r"(.*)",
-                ["I'm sorry, I didn't quite understand what you meant. Could you please rephrase yourself?",
-                 "I'm not sure what you're asking. Can you please provide more context?"]
+                ["Apologies, but I'm having trouble grasping the meaning of your message. Could you please rephrase it?",
+                 "I'm afraid I didn't quite catch that. Could you provide more context or rephrase your statement?",
+                 "Hmm, I'm having difficulty understanding what you're saying. Could you please clarify or phrase it differently?",
+                 "I'm sorry, but I'm having trouble making sense of your message. Could you please rephrase it or provide more information?",
+                 "It seems I'm unable to comprehend your message at the moment. Could you please rephrase it or give me more details?"]
             ]
         ]
         # Initialize the chatbot
@@ -180,20 +214,20 @@ class Quantron:
                 # Let the command be in this format: "translate <text> to <language>"
                 parts = command.split("translate")
                 if len(parts) < 2:
-                    response = "Please provide the text and target language for translation."
+                    response = "Please provide the text and target language for translation in the following format: 'translate <text> to <language>'"
                 else:
                     text = parts[1].strip()
                     if 'to' in text:
                         text_parts = text.split("to")
                         if len(text_parts) < 2:
-                            response = "Please provide the target language for translation."
+                            response = "Please provide the text and target language for translation in the following format: 'translate <text> to <language>'"
                         else:
                             text = text_parts[0].strip()
                             target_language = text_parts[1].strip()
                             translation = self.translate_text(text, target_language)
                             response = f"The translation of '{text}' to {target_language} is: '{translation}'"
                     else:
-                        response = "Please provide the target language for translation."
+                        response = "Please provide the text and target language for translation in the following format: 'translate <text> to <language>'"
             # Predict gender
             elif 'gender' in command:
                 name = command.split('gender')[-1].strip()
@@ -206,8 +240,8 @@ class Quantron:
             elif 'activity' in command:
                 response = self.get_random_activity()
             # Make a calculation
-            elif 'what is' in command:
-                expression = command.split('what is ')[-1]
+            elif 'calculate' in command:
+                expression = command.split('calculate ')[-1]
                 expression = expression.replace('plus', '+')
                 expression = expression.replace('minus', '-')
                 expression = expression.replace('times', '*')
@@ -218,12 +252,12 @@ class Quantron:
                     result = eval(expression)
                     response = f"The answer is {result}"
                 except Exception as e:
-                    response = "Sorry, I couldn't calculate that."
+                    response = "Regrettably, I was unable to perform the calculation."
             # Use chatbot to generate response
             else:
                 response = self.chatbot.respond(command)
         except Exception as e:
-            response = "An error occurred while processing your request. Please try again."
+            response = "Apologies for the inconvenience caused. An error occurred during the processing of your request. Please try again."
 
         print(response)
         return response
@@ -235,4 +269,3 @@ class Quantron:
 if __name__ == '__main__':
     assistant = Quantron()
     assistant.run()
-
